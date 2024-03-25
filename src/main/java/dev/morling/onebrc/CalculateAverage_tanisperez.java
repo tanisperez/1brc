407ea417f32567c0c0aa5c7cc735d5be3f489e9d
Tanis Pérez Nartallo <tanisperez@gmail.com>
2024-03-25 19:40:53 +0100

    WIP producer-consumer implementation

--- src/main/java/dev/morling/onebrc/CalculateAverage_tanisperez.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_tanisperez.java
@@ -15,14 +15,18 @@
  */
 package dev.morling.onebrc;
 
-import static java.util.stream.Collectors.*;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collector;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+import static java.nio.file.Files.newBufferedReader;
+import static java.util.stream.Collectors.groupingBy;
 
 public class CalculateAverage_tanisperez {
 
@@ -31,6 +35,25 @@ public class CalculateAverage_tanisperez {
     private static record Measurement(String station, double value) {
         private Measurement(String[] parts) {
             this(parts[0], Double.parseDouble(parts[1]));
+        }
+
+        public static Measurement from(final String line) {
+            boolean readingValue = false;
+            final StringBuilder station = new StringBuilder(line.length());
+            final StringBuilder value = new StringBuilder(line.length());
+            for (int i = 0; i < line.length(); i++) {
+                final char character = line.charAt(i);
+                if (character == ';') {
+                    readingValue = true;
+                    continue;
+                }
+                if (readingValue) {
+                    value.append(character);
+                } else {
+                    station.append(character);
+                }
+            }
+            return new Measurement(station.toString(), Double.parseDouble(value.toString()));
         }
     }
 
@@ -53,40 +76,81 @@ public class CalculateAverage_tanisperez {
     }
 
     public static void main(String[] args) throws IOException {
-        // Map<String, Double> measurements1 = Files.lines(Paths.get(FILE))
-        // .map(l -> l.split(";"))
-        // .collect(groupingBy(m -> m[0], averagingDouble(m -> Double.parseDouble(m[1]))));
-        //
-        // measurements1 = new TreeMap<>(measurements1.entrySet()
-        // .stream()
-        // .collect(toMap(e -> e.getKey(), e -> Math.round(e.getValue() * 10.0) / 10.0)));
-        // System.out.println(measurements1);
+        final int numberOfCores = Runtime.getRuntime().availableProcessors();
+        var executorsService = Executors.newFixedThreadPool(numberOfCores + 1);
 
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
+        final AtomicBoolean eof = new AtomicBoolean(false);
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
-                });
+        Map<String, MeasurementAggregator> results = new ConcurrentHashMap<>(10_000);
 
-        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
-                .map(l -> new Measurement(l.split(";")))
-                .collect(groupingBy(m -> m.station(), collector)));
+        Runnable producer = () -> {
+            try (BufferedReader reader = newBufferedReader(Paths.get(FILE), StandardCharsets.UTF_8)) {
+                for (;;) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        eof.getAndSet(true);
+                        break;
+                    }
+                    queue.add(line);
+                }
+            }
+            catch (final Exception exception) {
+                exception.printStackTrace();
+            }
+        };
 
-        System.out.println(measurements);
+        Runnable consumer = () -> {
+            try {
+                while (!eof.get()) {
+                    final String line = queue.take();
+                    final Measurement measurement = Measurement.from(line);
+
+                    MeasurementAggregator measurementAggregator = new MeasurementAggregator();
+                    measurementAggregator.min = measurement.value;
+                    measurementAggregator.max = measurement.value;
+                    measurementAggregator.sum = measurement.value;
+                    measurementAggregator.count = 1;
+
+                    results.merge(measurement.station, measurementAggregator,
+                            (currentValue, newValue) -> {
+                                MeasurementAggregator newMeasurementAggregator = new MeasurementAggregator();
+                                newMeasurementAggregator.min = Math.min(currentValue.min, newValue.min);
+                                newMeasurementAggregator.max = Math.max(currentValue.max, newValue.max);
+                                newMeasurementAggregator.sum = currentValue.sum + newValue.sum;
+                                newMeasurementAggregator.count = currentValue.count + 1;
+                                return newMeasurementAggregator;
+                            });
+                }
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        try {
+            executorsService.submit(producer);
+            for (int i = 0; i < numberOfCores; i++) {
+                executorsService.submit(consumer);
+            }
+            executorsService.shutdown();
+            executorsService.awaitTermination(10, TimeUnit.SECONDS);
+        }
+        catch (final Exception exception) {
+            exception.printStackTrace();
+        }
+
+        Map<String, ResultRow> copy = results.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> new ResultRow(e.getValue().min, (Math.round(e.getValue().sum * 10.0) / 10.0) / e.getValue().count, e.getValue().max))
+                );
+
+        TreeMap<String, ResultRow> out = new TreeMap<>();
+        for (java.util.Map.Entry<String, ResultRow> result : copy.entrySet()) {
+            out.put(result.getKey(), result.getValue());
+        }
+
+        System.out.println(out);
     }
 }
